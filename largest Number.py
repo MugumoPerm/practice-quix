884662170e8ac4361d244739ce42a538bc6ad6af
MugumoPerm <perminusk21@gmail.com>
2023-05-19 14:54:25 +0300

    update

--- largest Number.py
+++ largest Number.py
@@ -5,20 +5,22 @@ from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         length = len(nums)
-        num_count = []
+        num_count = 0
+        atleast_twice = 2
         for i in range(length):
             present_twice = nums.count(nums[i])
-            num_count.append(present_twice)
-        if 2 in num_count:
+            if present_twice >= 2:
+                num_count += 1
+        if num_count >0:
             return True
         else:
             return False
-
+                
     
         
         
         
-nums = ([1,2,3,4,5])        
+nums = ([7,10,5,5,6,6,4,10,5,4,9,4,9,6,5,9,6,3,6,5,6,7,7,4,9,9,10,5,8,1,8,3,2,7,5,10,1,8,5,8,4,3,6,4,9,4,2,8,3,2,2,1,5,6,3,2,6,1,8,6,2,9,1,4,5,10,8,5,10,5,10,1,4,8,3,6,4,10,9,1,1,1,2,2,9,6,6,8,1,9,2,5,5,2,1,8,5,2,3,10])        
 large = Solution()
 
 print(large.containsDuplicate(nums))
