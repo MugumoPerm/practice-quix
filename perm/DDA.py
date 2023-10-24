861f729b60b60eddade48fc31c3c8673cb3d0aa2
MugumoPerm <perminusk21@gmail.com>
2023-10-24 07:42:38 +0300

    update DDA and unfinished brensenham

--- perm/DDA.py
+++ perm/DDA.py
@@ -1,4 +1,8 @@
+import matplotlib.pyplot as plt
+
 def DDA(x1, x2, y1, y2):
+   xp = []
+   yp = []
    # Get the steps
    dx = x2-x1
    dy = y2-y1
@@ -14,16 +18,41 @@ def DDA(x1, x2, y1, y2):
    x_inc = dx/step
 
    #create a loop with a range of steps while adding the increment values starting from (x1, y1)
-   for i in range(step):
-        x1 += x_inc
-        y1 += y_inc
+   
+   if step < 0:
+       inc = -1*step
+   else:
+        inc = step
+   for i in range(inc):
+        if step < 0:
+            x1 -= x_inc
+            y1 -= y_inc
+        else:
+           x1 += x_inc
+           y1 += y_inc
 
+        xp.append(round(x1))
+        yp.append(round(y1))
         # print the results and round them to the nearest whole number
         print(round(x1), round(y1))
+   # create a scatter plot      
+   plt.scatter(xp, yp, label='Data Points', color='b', marker='o')
+   
+   plt.xlabel('X-axis')
+   plt.ylabel('Y-axis')
+   plt.title('Scatter Plot Example')
+
+   # Add a legend
+   plt.legend()
+
+   # Display the plot
+   plt.show()
+
+   
 
 # Get the two points
-x1,y1 = (20, 10)
+x1,y1 = (12, 9)
 
-x2,y2 = (30, 18)
+x2,y2 = (17, 14)
 
 DDA(x1, x2, y1, y2)
--- perm/brensenhams.py
+++ perm/brensenhams.py
@@ -1,4 +1,25 @@
-def bren():
+# def bren():
+import matplotlib.pyplot as plt
+
+# Sample data - replace with your coordinates
+x = [1, 2, 3, 4, 5]
+y = [10, 12, 5, 8, 15]
+
+# Create a scatter plot
+plt.scatter(x, y, label='Data Points', color='b', marker='o')
+
+# Add labels and a title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Scatter Plot Example')
+
+# Add a legend
+plt.legend()
+
+# Display the plot
+plt.show()
 
 
-bren()
+
+
+# bren()
